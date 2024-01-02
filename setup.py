ff9e83b523d550d9799bd996c4188d171d7f175b
Onyiriuba Leonard <leonardleo110011@gmail.com>
2024-01-02 09:53:12 +0000

    Updates

--- setup.py
+++ setup.py
@@ -27,10 +27,18 @@ setup(
     classifiers=[
         "Development Status :: 3 - Test Release",
         "Intended Audience :: Testers",
-        "Programming Language :: Python :: 3",
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         "Operating System :: Unix",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Microsoft :: Windows",
     ],
-    zip_safe=False
+    zip_safe=False,
+    python_requires='>=3.0',
 )
