36489a0b32d53c52f7105b69e4d8e0b67d71bf7c
Onyiriuba Leonard <leonardleo110011@gmail.com>
2024-01-08 02:04:38 +0000

    Updates

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt', 'r') as f:
     install_requires = f.read().splitlines()
 
 
-VERSION = '0.0.1'
+VERSION = '1.0.0'
 DESCRIPTION = 'Building machine learning models has never been easier.'
 LONG_DESCRIPTION = 'The ability to perform exploratory data analysis, data preprocessing, data cleaning, data transformation, data segregation, model training, model prediction, and model evaluation has been put together in this package to allow simple flow of ML operations.'
 HERE = pathlib.Path(__file__).parent
@@ -22,13 +22,11 @@ setup(
     author="TechLeo (Onyiriuba Leonard Chukwubuikem)",
     author_email="<techleo.ng@outlook.com>",
     description=DESCRIPTION,
-    # long_description=README,
     long_description_content_type="text/markdown",
-    long_description=LONG_DESCRIPTION,
+    long_description=README,
     packages=find_packages(),
-    # install_requires=['pandas', 'numpy', 'matplotlib', 'seaborn', 'scikit-learn'],
     install_requires=install_requires,
-    keywords = ['machine learning', 'data science', 'python library', 'deep learning', 'data analysis', 'predictive modeling', 'feature engineering', 'model training', 'algorithm implementation', 'data preprocessing', 'open source', 'neural networks', 'supervised learning', 'unsupervised learning', 'model evaluation', 'ensemble learning', 'AI development', 'data exploration', 'ML framework', 'data cleaning', 'big data analytics', 'model deployment', 'natural language processing', 'computer vision', 'regression', 'classification', 'clustering', 'hyperparameter tuning', 'cross-validation', 'machine learning toolkit'],
+    keywords = ['machine learning', 'data science', 'data preprocessing', 'supervised learning', 'data exploration', 'ML framework', 'data cleaning', 'regression', 'classification', 'machine learning toolkit'],
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: End Users/Desktop",
